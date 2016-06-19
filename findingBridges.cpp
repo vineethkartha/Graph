4614f09eb883a0b906de2e72dcb70e5d2095fb66
vineethkartha <vineeth.kartha@gmail.com>
2016-06-19 11:57:39 +0530

    A working code to find the bridges in a graph

--- findingBridges.cpp
+++ findingBridges.cpp
@@ -3,7 +3,7 @@
 #include<boost/graph/graphviz.hpp>
 #include <boost/graph/connected_components.hpp>
 
-typedef boost::adjacency_list<> boostGraph;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS>boostGraph;
 
 typedef boostGraph::vertex_iterator vIterator;
 typedef boostGraph::edge_iterator eIterator;
@@ -47,14 +47,37 @@ void Graph::findBridge() {
 3) If the above two numbers are the same then the edge is not a a bridge
 4) Proceed untill all edges are checked.
   */
-  std::vector<int> component (boost::num_vertices (graph));
-  int s = boost::connected_components(graph,&component[0]);
-  std::cout<<"The count is: "<<s<<std::endl;
+  int s;
+  auto temp = graph;
+  std::vector<int> component (boost::num_vertices (temp));
+  int origComp = boost::connected_components(graph,&component[0]);
+  std::tie(start,end) = boost::edges(temp);
+  int n = boost::num_edges(temp);
+  int i =0;
+  auto ed =start;
+  auto t = ed;
+  while(i<n) {
+    auto src = boost::source(*ed,temp);
+    auto tar = boost::target(*ed,temp);
+    t = ed;
+    ed++;
+    remove_edge(*t,temp);
+    s = boost::connected_components(temp,&component[0]);
+    auto e = add_edge(src,tar,temp);
+    if(s>origComp) {
+      std::cout<<e.first<<"Is a Bridge\n";
+    }
+    i++;
+  }
 }
 
 int main() {
   Graph g;
+  std::ofstream myfile;
   g.readGraph();
   g.findBridge();
+  myfile.open("sam.dot");
+  g.printGraph(myfile);
+  myfile.close();  
   return 0;
 }
