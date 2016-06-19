fd9978b994efdea4263ed260b3f3db00913ee429
vineethkartha <vineeth.kartha@gmail.com>
2016-06-19 12:24:24 +0530

    Tidying up of the code

--- findingBridges.cpp
+++ findingBridges.cpp
@@ -4,12 +4,7 @@
 #include <boost/graph/connected_components.hpp>
 
 typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS>boostGraph;
-
-typedef boostGraph::vertex_iterator vIterator;
 typedef boostGraph::edge_iterator eIterator;
-
-typedef boostGraph::vertex_descriptor vDescriptor;
-typedef boostGraph::edge_descriptor eDescriptor;
 
 
 class Graph {
@@ -39,35 +34,37 @@ void Graph::printGraph(std::ofstream &file) {
   boost::write_graphviz(file,graph);
 }
 
-void Graph::findBridge() {
-  /**
+/**
 
 1) take an edge and find num of connected components.
 2) remove that edge and again find number of components
 3) If the above two numbers are the same then the edge is not a a bridge
 4) Proceed untill all edges are checked.
-  */
-  int s;
-  auto temp = graph;
-  std::vector<int> component (boost::num_vertices (temp));
-  int origComp = boost::connected_components(graph,&component[0]);
-  std::tie(start,end) = boost::edges(temp);
-  int n = boost::num_edges(temp);
-  int i =0;
-  auto ed =start;
-  auto t = ed;
-  while(i<n) {
-    auto src = boost::source(*ed,temp);
-    auto tar = boost::target(*ed,temp);
-    t = ed;
-    ed++;
-    remove_edge(*t,temp);
-    s = boost::connected_components(temp,&component[0]);
-    auto e = add_edge(src,tar,temp);
-    if(s>origComp) {
-      std::cout<<e.first<<"Is a Bridge\n";
+*/
+
+void Graph::findBridge() {
+
+  int numOfComponents = 0;
+  std::vector<int> component (boost::num_vertices (graph));
+  int numOfCompInGraph = boost::connected_components(graph,&component[0]);
+
+  std::tie(start,end) = boost::edges(graph);
+  int numOfEdges = boost::num_edges(graph);
+  int edgeIndex = 0;
+  auto temp = start;
+  
+  while(edgeIndex < numOfEdges) {
+    auto src = boost::source(*start,graph);
+    auto tar = boost::target(*start,graph);
+    temp = start;
+    start++;
+    remove_edge(*temp,graph);
+    numOfComponents = boost::connected_components(graph,&component[0]);
+    auto edge = add_edge(src,tar,graph);
+    if(numOfComponents > numOfCompInGraph) {
+      std::cout<<edge.first<<"Is a Bridge\n";
     }
-    i++;
+    edgeIndex++;
   }
 }
 
@@ -76,7 +73,7 @@ int main() {
   std::ofstream myfile;
   g.readGraph();
   g.findBridge();
-  myfile.open("sam.dot");
+  myfile.open("sam.dot");  
   g.printGraph(myfile);
   myfile.close();  
   return 0;
